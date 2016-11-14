6037c6b3d53572e6ab8930fb1ae6699d0c36fc34
zyuzhi <tearsxy@gmail.com>
2016-11-14 20:40:45 +0800

    update sample file

--- sample.cpp
+++ sample.cpp
@@ -24,9 +24,9 @@ void test1()
 	a.push_back(std::make_pair(4, "f"));
 	a.push_back(std::make_pair(4, "g"));
 
-	auto res = From(a)
-		.Where([](inner_type i) { return i.first > 1; })
-		.Select([](inner_type i) { return i.second; });
+	auto res = from(a)
+		.where([](inner_type i) { return i.first > 1; })
+		.select([](inner_type i) { return i.second; });
 	for (auto it = res.begin(); it != res.end(); ++it)
 	{
 		std::cout << *it << std::endl;
@@ -36,16 +36,16 @@ void test1()
 void test2()
 {
 	int a[10] = {10, 9, 8, 7, 6, 5, 4, 3, 2, 1};
-	auto res = From(a).Where([](int i){return i > 5;});
+	auto res = from(a).where([](int i){return i > 5;});
 	for (auto it = res.begin(); it != res.end(); ++it)
 	{
 		std::cout << *it << std::endl;
 	}
-	std::cout << "all > 8 " << res.All([](int i){return i > 8;}) << std::endl;
-	std::cout << "any > 8 " << res.Any([](int i){return i > 8;}) << std::endl;
-	std::cout << "count > 8 " << res.Count([](int i){return i > 8;}) << std::endl;
-	std::cout << "count " << res.Count() << std::endl;
-	std::cout << "sum " << res.Sum() << std::endl;
+	std::cout << "all > 8 " << res.all([](int i){return i > 8;}) << std::endl;
+	std::cout << "any > 8 " << res.any([](int i){return i > 8;}) << std::endl;
+	std::cout << "count > 8 " << res.count([](int i){return i > 8;}) << std::endl;
+	std::cout << "count " << res.count() << std::endl;
+	std::cout << "sum " << res.sum() << std::endl;
 }
 
 int main()
